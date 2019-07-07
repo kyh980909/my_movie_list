a1f10a707e8c9f420b03cf28c329ffcb270c4a9e
Yongho Kim <kyh980909@gmail.com>
2019-07-08 00:20:13 +0900

    Movie, Database 클래스 생성

---
+++ lib/Database/DBHelper.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:my_movie_list/Model/movie.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DBHelper {
+  final String tableName = "movie_info";
+  static Database dbInstance;
+
+  Future<Database> get db async {
+    if (dbInstance == null) dbInstance = await initDB();
+    return dbInstance;
+  }
+
+  initDB() async {
+    io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "movie.db");
+
+    var db = await openDatabase(path, version: 1, onCreate: onCreateFunc);
+    return db;
+  }
+
+  void onCreateFunc(Database db, int version) async {
+    await db.execute(
+        'create table $tableName(id integer primary key autoincrement, title text, date text);');
+  }
+
+
+  // DB 에서 영화 목록 불러오기
+  Future<List<Movie>> getMovies() async {
+    var dbCon = await db;
+    List<Map> list = await dbCon.rawQuery('select * from $tableName');
+    List<Movie> movies = List();
+
+    for (int i = 0; i < list.length; i ++) {
+      Movie movie = new Movie();
+      movie.id = list[i]['id'];
+      movie.title = list[i]['title'];
+      movie.date = list[i]['date'];
+
+      movies.add(movie);
+    }
+
+    return movies;
+  }
+
+  void addMovie(Movie movie) async {
+    var dbCon = await db;
+    String sql = 'insert into $tableName(title, date) values ("${movie.title}","${movie.date}")';
+    await dbCon.transaction((transaction) async {
+      return await transaction.rawInsert(sql);
+    });
+  }
+
+  void updateMoive(Movie movie) async {
+    var dbCon = await db;
+    String sql = 'update $tableName set name = "${movie.title}, ${movie.date}" where id=${movie.id}';
+    await dbCon.transaction((transaction) async {
+      return await transaction.rawQuery(sql);
+    });
+  }
+
+  void deleteMoive(Movie movie) async {
+    var dbCon = await db;
+    String sql = 'delete from $tableName where id=${movie.id}';
+    await dbCon.transaction((transaction) async {
+      return await transaction.rawQuery(sql);
+    });
+  }
+}
---
+++ lib/Model/movie.dart
@@ -0,0 +1,7 @@
+class Movie {
+  int id; // 인덱스
+  String title; // 영화명
+  String date; // 영화를 본 날짜
+
+  Movie();
+}
